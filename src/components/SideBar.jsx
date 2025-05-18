d9a51ec9ec7346d000a73d17add1e8eb1dc794c7
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-05-18 18:47:27 +0330

    Add Nested Router

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,11 +1,15 @@
 import styles from './Sidebar.module.css';
 import AppNav from "./AppNav.jsx";
 import Logo from "./Logo.jsx";
+import {Outlet} from "react-router-dom";
 
 function SideBar() {
     return <div className={styles.sidebar}>
         <Logo/>
         <AppNav/>
+
+        <Outlet />
+
         <footer className={styles.footer}>
             <p className={styles.copyright}>
                 &copy; CopyRight {new Date().getFullYear()} by World Wise inc.
