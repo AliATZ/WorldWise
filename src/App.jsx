7248b07f1b68ac047f1fa8907b956024cd0d9a7c
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-05-23 16:29:23 +0330

    Add Dynamic Routes With Parameters

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Login from "./pages/Login.jsx";
 import CityList from "./components/CityList.jsx";
 import {useEffect, useState} from "react";
 import CountriesList from "./components/CountriesList.jsx";
+import City from "./components/City.jsx";
 
 const BASE_URL='http://localhost:9000';
 
@@ -41,6 +42,7 @@ export default function App(){
             <Route path="app" element={<AppLayout/>}>
                 <Route index element={<CityList cities={cities} isLoading={isLoading}/>}/>
                 <Route path='cities' element={<CityList cities={cities} isLoading={isLoading} />}/>
+                <Route path='cities/:id' element={<City/>}/>
                 <Route path='countries' element={<CountriesList cities={cities} isLoading={isLoading} />}/>
                 <Route path='form' element={<p>List of form</p>}/>
             </Route>
