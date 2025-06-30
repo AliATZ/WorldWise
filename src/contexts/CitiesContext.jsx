b04c748f8b752cce56a67b8ae88ee41257de0bc8
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-06-30 21:27:04 +0330

    Create Form for City Component

--- src/contexts/CitiesContext.jsx
+++ src/contexts/CitiesContext.jsx
@@ -6,6 +6,8 @@ const BASE_URL='http://localhost:9000';
 function CitiesProvider({children}) {
     const [cities, setCities] = useState([]);
     const [isLoading, setIsLoading] = useState(false);
+    const [currentCity, setCurrentCity] = useState({})
+
     useEffect(function (){
         async function fetchCities(){
             try {
@@ -23,9 +25,25 @@ function CitiesProvider({children}) {
         fetchCities();
     },[])
 
+    async function getCity(id){
+            try {
+                setIsLoading(true);
+                const res = await fetch(`${BASE_URL}/cities/${id}`);
+                const data = await res.json();
+                setCurrentCity(data);
+            }catch {
+                // alert('could not load cities.');
+            }finally {
+                setIsLoading(false);
+            }
+
+    }
+
     return <CitiesContext.Provider value={{
         cities,
         isLoading,
+        currentCity,
+        getCity
     }}>{children}</CitiesContext.Provider>;
 
 }
