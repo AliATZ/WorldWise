939fec4a932df3e5a3611a2e4d1ad59e3633c86d
Ali Atazadeh <aliiiatzzz1382@gmail.com>
2025-07-14 23:48:38 +0330

    feat(Map): Add Interaction With Map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,18 +1,23 @@
 import styles from './Map.module.css'
 import {useNavigate, useSearchParams} from "react-router-dom";
-import {MapContainer, TileLayer,Marker,Popup} from "react-leaflet";
-import {useState} from "react";
+import {MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents} from "react-leaflet";
+import {useEffect, useState} from "react";
 import {useCities} from "../contexts/CitiesContext.jsx";
 
 function Map(){
-    const navigate= useNavigate()
     const {cities} =useCities()
 
     const [mapPosition, setMapPosition] = useState([40,0])
-    const [searchPrams,setSearchPrams]=useSearchParams()
-    const lat = searchPrams.get('lat');
-    const lng = searchPrams.get('lng');
-    return <div className={styles.mapContainer} onClick={()=>{navigate("form")}}>
+    const [searchParams , setSearchParams] = useSearchParams();
+    const mapLat = searchParams.get('lat');
+    const mapLng = searchParams.get('lng');
+
+    useEffect(() => {
+        if (mapLat && mapLng) {
+            setMapPosition([mapLat, mapLng])
+        }
+    },[mapLat,mapLng]);
+    return <div className={styles.mapContainer}>
         <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.map}>
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -25,7 +30,22 @@ function Map(){
                     <span>{city.cityName}</span>
                 </Popup>
             </Marker>))}
+            <ChangeCenter position={mapPosition}/>
+            <DetectClick/>
         </MapContainer>
     </div>
+}
+
+function ChangeCenter({position}){
+    const map=useMap()
+    map.setView(position);
+    return null;
+}
+
+function DetectClick(){
+    const navigate= useNavigate()
+    useMapEvents({
+        click: (e)=> navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
+    })
 }
 export default Map;
